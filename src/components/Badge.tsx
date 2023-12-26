06f392e987fb543a4de192092066b96a25d8c9c6
Anirudh Asher <75933833+anirudhasher@users.noreply.github.com>
2023-12-26 17:20:04 +0530

    Update Badge.tsx

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -50,17 +50,17 @@ export const Badge = (props: Props) => {
         'background-color': props.badgeBackgroundColor ?? '#ffffff',
       }}
     >
-      
+      Powered by
       <a
         ref={liteBadge}
-        href={'https://flowiseai.com'}
+        href={'https://AtomicAsher.com'}
         target="_blank"
         rel="noopener noreferrer"
         class="lite-badge"
         id="lite-badge"
         style={{ 'font-weight': 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> </span>
+        <span> Atomic Asher LLP </span>
       </a>
     </span>
   );
